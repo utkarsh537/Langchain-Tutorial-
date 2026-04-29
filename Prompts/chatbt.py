4a811bc8ec2d4b51c23a93433302a397d63e96ca
Utkarsh Pandey <utkarshmahichsbhu@gmail.com>
2026-04-30 02:31:19 +0530

    Add files via upload

---
+++ Prompts/chatbt.py
@@ -0,0 +1,16 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.Message import SystemMessage, HumanMessage, AIMessage
+from dotenv import load_dotenv
+load_dotenv()
+model = ChatOpenAI()
+chat_history = [SystemMessage(content="You are a helpful assistant.")]
+while True:    
+    user_input = input("You: ")
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == "exit":
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("ChatGPT: ", result.content)
+    
+print(chat_history)
---
+++ Prompts/message.py
@@ -0,0 +1,14 @@
+from langchain_core.Message import SystemMessage, HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+model = ChatOpenAI()
+
+messages = [
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="Tell me about LangChain.")
+]
+result = model.invoke(messages)
+messages.append(AIMessage(content=result.content))
+print(messages)
